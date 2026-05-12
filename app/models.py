80a63735a815056dcaa6cbc6467d5ab87487dfa1
Lena164-star <rlena6449@gmail.com>
2026-05-12 15:05:29 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,15 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, func
-from .db import Base
+from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy.sql import func
+from .database import Base
 
 class RequestHistory(Base):
     __tablename__ = "requests_history"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     input_text = Column(Text, nullable=False)
-    result_text = Column(String, nullable=False)   # например, "spam" или "ham"
+    result_text = Column(Text, nullable=False) # Будем хранить JSON-строку
     model_name = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return f"<RequestHistory(id={self.id}, model='{self.model_name}')>"
