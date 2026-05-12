ac87366381d3431f674c823a13d7396c0b3f9cd2
Lena164-star <rlena6449@gmail.com>
2026-05-12 15:10:40 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,16 +1,27 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    app_name: str = "AI Spam Detector"
+    # Hugging Face model
     huggingface_model_name: str = "mrm8488/bert-tiny-finetuned-sms-spam-detection"
 
+    # PostgreSQL
     postgres_user: str
     postgres_password: str
     postgres_db: str
-    postgres_host: str
+    postgres_host: str = "localhost"
     postgres_port: int = 5432
 
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql://{self.postgres_user}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
+
     class Config:
-        env_file = ".env" # Путь к файлу с переменными окружения
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
 
 settings = Settings()
